47657c61747a9b075d26b87043f23c3d056ed43b
Bryan <bsg262@gmail.com>
2022-06-06 18:06:37 -0400

    cleaned up code to readability

--- KnowledgeBelief/utils.py
+++ KnowledgeBelief/utils.py
@@ -4,37 +4,10 @@ import json
 import random
 
 
-def randomize_trials():
-    trial_types = ['test', 'distractor']
-    vig_numbers = list(range(1, 13))
-    bel_types = ['TB', 'FB', 'IG']
-    ascrip_types = ['Knows', 'Thinks']
-    # assign scenarios and shuffle
-    s1 = pd.DataFrame({
-        'trial_num': None,
-        'prompt': None,
-        'correct': None,
-        'trial_type': 'test',
-        'scenario': vig_numbers,
-        'belief': None,
-        'ascription': None,
-        'target': None,
-        'crrct_answer': None,
-        'target_onset': None,
-        'response_onset': None,
-        'response_key': None,
-    }).sample(frac=1)
-    # balance balance acription types and shuffly
-    s1['ascription'] = np.repeat(ascrip_types, len(vig_numbers) / len(ascrip_types))
-    s1 = s1.sample(frac=1)
-    # balance belief types and shuffl
-    s1['belief'] = np.repeat(bel_types, len(vig_numbers) / len(bel_types))
-    s2 = s1.copy()
-    s2['trial_type'] = 'distractor'
-    s3 = pd.concat([s1, s2], ignore_index=True).sample(frac=1).reset_index(drop=True)
-    s3['trial_num'] = range(1, len(s3) + 1)
-    # add practice
-    # Adding Practice
+# practice
+def make_practice():
+    n_tf_pract = 8
+    n_story_pract = 2
     p1 = [
         "Kevin is out grocery shopping at the local market. His wife told him to get sourdough bread and pasta for dinner that evening.",
         "At the market, Kevin picks up some pasta, but he cannot find any sourdough bread anywhere. He looks up and down every aisle and then finally asks someone who works at the market.",
@@ -49,30 +22,75 @@ def randomize_trials():
         "Sara doesn't lend the scooter to her sister. Her sister walks to dance class and is late."
         ]
     p2_target = "Sara wanted to take her scooter to the park."
-    n_practice = 8
-    s3a = pd.DataFrame({x: None for x in s3.columns.tolist()}, index=range(n_practice))
-    s3a.trial_type = ['tf_practice', 'tf_practice', 'tf_practice', 'tf_practice', 'tf_practice', 'tf_practice',
-                      'practice', 'practice']
-    s3a.trial_num = [999] * n_practice
-
-    tf_prac = ["False"] * 3 + ["True"] * 3
+    tf_prac = ["False"] * int(n_tf_pract/2) + ["True"] * int(n_tf_pract/2)
     random.shuffle(tf_prac)
     ca = ['f' if tf_p == 'False' else 'j' for tf_p in tf_prac]
     [ca.append(x) for x in ['j', 'f']]
-    [tf_prac.append(prac_prompt) for prac_prompt in [p1, p2]]
-    pr_targ = [None] * 6
-    [pr_targ.append(p_target) for p_target in [p1_target, p2_target]]
+    pract = {'trial_num': list(range(1, sum([n_tf_pract,n_story_pract])+1)),
+             'trial_type':['tf_practice'] * n_tf_pract + ['story_practice']* n_story_pract,
+             'prompt': tf_prac + [p1, p2],
+             'correct': None,
+             'target': [None]*n_tf_pract + [p1_target, p2_target],
+             'correct_answer': ca,
+             'response_key':None,
+             'target_onset': None,
+             'response_onset': None,
+             }
+    return pd.DataFrame(pract)
 
-    s3a.prompt = tf_prac
-    s3a.target = pr_targ
-    s3a.crrct_answer = ca
-    s4 = s3a.append(s3, ignore_index=True)
-    xx = s4.loc[s4.trial_type == 'test'].sample(frac=1)
-    fel = pd.DataFrame({'b2_trial_num': range(1, 13),
-                        'b1_trial_num': [int(x) for x in xx.trial_num.values],
-                        'fel_scenario': [int(x) for x in xx.scenario.values],
+
+# Trials
+def make_trials():
+    vig_numbers = list(range(1, 13))
+    bel_types = ['TB', 'FB', 'IG']
+    ascrip_types = ['Knows', 'Thinks']
+    # assign scenarios and shuffle
+    s1 = pd.DataFrame({
+        'trial_num': None,
+        'correct': None,
+        'trial_type': 'test',
+        'scenario': vig_numbers,
+        'belief': None,
+        'ascription': None,
+        'crrct_answer': None,
+        'target_onset': None,
+        'response_onset': None,
+        'response_key': None,
+    }).sample(frac=1)
+    # balance ascription types and shuffle
+    s1['ascription'] = np.repeat(ascrip_types, len(vig_numbers) / len(ascrip_types))
+    s1 = s1.sample(frac=1)
+    # balance belief types and shuffle
+    s1['belief'] = np.repeat(bel_types, len(vig_numbers) / len(bel_types))
+    # add distractors
+    s2 = s1.copy()
+    s2['trial_type'] = 'distractor'
+    s3 = pd.concat([s1, s2], ignore_index=True).sample(frac=1).reset_index(drop=True)
+    s3['trial_num'] = range(1, len(s3) + 1)
+    # add correct answers
+    json_fp = 'KnowledgeBelief/static/stim_data/KB_stim.json'
+    with open(json_fp, 'r') as j:
+        stim = json.loads(j.read())
+    for i, trl in s3.iterrows():
+        s3.loc[s3.trial_num == trl.trial_num, 'crrct_answer'] = stim[trl.trial_type][str(trl.scenario)]['ascription'][trl.ascription]['crrct_answr']
+    return s3
+
+
+# Felicity
+def make_felicity(trls):
+    xx = trls.loc[trls.trial_type == 'test'].sample(frac=1)
+    fel = pd.DataFrame({'b2_trial_num': list(range(1, 13)),
+                        'b1_trial_num': xx.trial_num.values,
+                        'fel_scenario': xx.scenario.values,
                         'fel_belief_type': xx.belief.values,
                         'fel_ascription_type': xx.ascription.values,  # "knows" or "thinks"
                         'felicity_rating': None})
+    return fel
 
-    return s4, fel
+
+# all subject trials
+def randomize_trials():
+    prac = make_practice()
+    trls = make_trials()
+    fels = make_felicity(trls)
+    return prac, trls, fels
