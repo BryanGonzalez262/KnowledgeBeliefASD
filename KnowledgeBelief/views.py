b450620768fad86a8abb56b7badcddba2d3a74f9
Bryan <bsg262@gmail.com>
2022-06-08 09:45:46 -0400

    refactored so all instructions use same template

--- KnowledgeBelief/views.py
+++ KnowledgeBelief/views.py
@@ -86,7 +86,13 @@ def new_subject():
 @app.route('/instructions', methods=['GET', 'POST'])
 def instructions():
     if request.method == 'GET':
-        return render_template('instructions.html')
+        title = "Welcome"
+        stim = ["In this study, we'd like you to make judgments about whether certain statements are true or false.",
+                "To indicate that something is false, you'll press the <b>[f]</b> key.",
+                "To indicate that something is true, please press the <b>[j]</b> key.",
+                'To get you used to this, you will first go through a warm-up phase. <br> On these trials, press <b>f</b> if you see the word "False" <br> and press <b>j</b> if you see the word "True."']
+        next = "/tf_practice"
+        return render_template('instruct.html', title=title, stim=stim, next=next)
     else:
         return make_response("200")
 
@@ -115,8 +121,7 @@ def tf_practice():
         db.session.add(tdat)
         db.session.commit()
 
-        return make_response("200") # redirect(url_for('fixation', PROLIFIC_PID=sub_dat['PROLIFIC_PID'], SESSION_ID=sub_dat['SESSION_ID']))
-
+        return make_response("200")
 
 @app.route('/fixation', methods=['GET', 'POST'])
 def fixation():
@@ -126,15 +131,28 @@ def fixation():
 
 @app.route('/task_instruct', methods=["GET", "POST"])
 def task_instruct():
-    return render_template('task_instruct.html')
+    title = "Task Instructions"
+    next = "/story"
+    stim = ["In this part of the study, you will be shown 24 short stories about different people and then you will have to answer a question about the story you read.",
+            "***Please pay very close attention and answer as quickly and accurately as you possibly can. If you don't respond within 5 seconds, we will go to the next question.***",
+            "Before starting there will be two practice examples of the sort of task you'll be doing. Remember, you'll have to read and respond quickly."]
+    return render_template('instruct.html', title=title, stim=stim, next=next)
 
 
 @app.route('/next_trial', methods = ['GET'])
 def next_trial():
     t = int(request.args.get('trial'))+1
     if request.args.get('exp_state') == "TF_TRIAL":
-        return redirect(url_for('ready', PROLIFIC_PID=request.args.get('PROLIFIC_PID'),
-                         SESSION_ID=request.args.get('SESSION_ID'), exp_state="TF_TRIAL", trial=t))
+        if t <= n_trials:
+            return redirect(url_for('ready', PROLIFIC_PID=request.args.get('PROLIFIC_PID'),
+                                    SESSION_ID=request.args.get('SESSION_ID'), exp_state="TF_TRIAL", trial=t))
+        else:
+            subj =Subject.query.filter_by(prolific_id=request.args.get('PROLIFIC_PID')).first()
+            subj.block1_complete = True
+            db.session.add(subj)
+            db.session.commit()
+            return redirect(url_for('ready', PROLIFIC_PID=request.args.get('PROLIFIC_PID'),
+                                    SESSION_ID=request.args.get('SESSION_ID'), exp_state="FELICITY_PRACTICE", trial=1))
 
     elif request.args.get('exp_state') == "TF_PRACTICE":
         return redirect(url_for('tf_practice', PROLIFIC_PID=request.args.get('PROLIFIC_PID'), SESSION_ID=request.args.get('SESSION_ID'),
@@ -151,14 +169,27 @@ def next_trial():
                                 SESSION_ID=request.args.get('SESSION_ID'), exp_state=state, trial=trl))
 
 
+
+msgs = {e_state: {var: None for var in [1, 2, 'next']} for e_state in ['TRIAL_PRACTICE', 'TF_TRIAL', 'FELICITY_PRACTICE']}
+
+msgs["TRIAL_PRACTICE"][1] = "Okay, one more practice example"
+msgs["TRIAL_PRACTICE"][2] = "Press the space bar to continue, then place your fingers on the [f] and [j] keys.... "
+msgs["TRIAL_PRACTICE"]['next'] = "/story"
+msgs["TF_TRIAL"][1] = "Okay, get ready for the next story." #"Okay, get ready for story #" + request.args.get('trial') + "."
+msgs["TF_TRIAL"][2] = "Press the space bar to continue, then place your fingers on the [f] and [j] keys.... "
+msgs["TF_TRIAL"]['next'] = "/story"
+msgs["FELICITY_PRACTICE"][1] = "Great Job!"
+msgs["FELICITY_PRACTICE"][2] = "Press the space bar to continue.... "
+msgs["FELICITY_PRACTICE"]['next'] = "/felicity_instr"
+
+
+
 @app.route('/ready', methods=['GET', 'POST'])
 def ready():
-    if request.args.get('exp_state') != "TRIAL_PRACTICE":
-        msg1 = "Okay, get ready for story #"+ request.args.get('trial')+"."
-    else:
-        msg1 = "Okay, one more practice example"
-    msg2 = "Press the space bar to continue, then place your fingers on the [f] and [j] keys.... "
-    return render_template('message.html', msg1=msg1, msg2=msg2, next="/story")
+
+    return render_template('message.html', msg1=msgs[request.args.get('exp_state')][1],
+                           msg2=msgs[request.args.get('exp_state')][2],
+                           next=msgs[request.args.get('exp_state')]['next'])
 
 
 @app.route('/story', methods=['GET', 'POST'])
@@ -204,8 +235,17 @@ def story():
 
         return make_response("200")
 
-
-
+@app.route('/felicity_instr')
+def felicity_instr():
+    title = "Great Job! Phase 2 Instructions"
+    stim = ["In the next part of the experiment you will be asked to judge whether things that people say sound weird or normal.",
+            "Here's an example of what we mean.<br>Suppose that your friend has three apples in front of him and after looking at them, he says, \"I have an apple in front of me.\"  <br>That's a weird thing to say.  It's true that there is a apple in front of him, but it sounds weird because there's not just one apple, there are actually three.  <br>A more normal thing to say would be \"I have three apples in front of me.\"",
+            "Here's another example.<br> Suppose that Mary got married and then had a baby two years later. Somebody then says, \"My friend Mary had a baby and got married.\"<br> Again, this is technically true because she did do both of those things.  But, it sounds weird because it seems to imply that she had the baby first and got married second, but she actually got married first and had the baby later. <br> A more normal thing to say would be \"Mary got married and had a baby.\"",
+            "Here's one last example.<br> Suppose Bruce accidentally hit his friend's hand with a hammer during a construction project.  Later somebody describes what happens by saying, \"Bruce broke a finger.\" <br> Again, this might be considered weird to say, because it seems to imply that Bruce broke his own finger, but he actually broke someone else's finger.  However, despite sounding weird, this sentence is technically true.",
+            "In this part of the experiment you will read a number of stories, and after each story we will give you a statement that somebody made about the story.  <br>Your job is to judge whether the statement sounds weird or normal.",
+            "Please try to remember that whether or not a sentence is true or false is a completely separate question from whether or not the sentence sounds normal or weird. <br>Some true sentences can sound weird, and some false sentences can sound normal.<br> Before starting, we want to make sure you understood the instructions by giving you some example questions."]
+    next="/story"
+    return render_template('instruct.html', title=title, stim=stim, next=next)
 
 if __name__ == '__main__':
     app.run()
