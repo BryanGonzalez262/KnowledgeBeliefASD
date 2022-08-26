aedbfbb0cd06f572472fa9758d81dcce29c70e5d
Bryan <bsg262@gmail.com>
2022-08-26 15:19:01 -0400

    added reCaptcha

--- KnowledgeBelief/__init__.py
+++ KnowledgeBelief/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_bootstrap import Bootstrap
+from flask_recaptcha import ReCaptcha
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dfkjfsdlf'
@@ -9,11 +10,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///knwlg_blf.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SESSION_COOKIE_SAMESITE'] = "None"
+app.config['RECAPTCHA_SITE_KEY'] = 'XXXXXXX'
+app.config['RECAPTCHA_SECRET_KEY'] = 'XXXXXXX'
 
 
 db = SQLAlchemy(app)
 cors = CORS(app)
 bootstrap = Bootstrap(app)
+recaptcha = ReCaptcha(app)
 
 
 from . import views
