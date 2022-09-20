0f7bbeb052c279b49c8945180746ce76c00a70cb
Bryan <bsg262@gmail.com>
2022-09-20 14:14:01 -0400

    initialize mail and config settings

--- KnowledgeBelief/__init__.py
+++ KnowledgeBelief/__init__.py
@@ -1,14 +1,17 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_bootstrap import Bootstrap
 from flask_recaptcha import ReCaptcha
 from flask_migrate import Migrate
+from flask_mail import Mail
 import json
 
 
 app = Flask(__name__)
-config_dir = "config.json"
+
 
 with open("KnowledgeBelief/config.json") as config_file:
     config = json.load(config_file)
@@ -20,6 +23,14 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = "False"
 app.config['SESSION_COOKIE_SAMESITE'] = "None"
 app.config['RECAPTCHA_SITE_KEY'] = config.get("RECAPTCHA_SITE_KEY")
 app.config['RECAPTCHA_SECRET_KEY'] = config.get("RECAPTCHA_SECRET_KEY")
+app.config['MAIL_SERVER'] = config.get("MAIL_SERVER")
+app.config['MAIL_PORT'] = config.get("MAIL_PORT")
+app.config['STUDY_MAIL_SUBJECT'] = config.get("STUDY_MAIL_SUBJECT")
+app.config['STUDY_MAIL_SENDER'] = config.get("STUDY_MAIL_SENDER")
+app.config['MAIL_USE_TLS'] = "True"
+app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USERNAME")
+app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")
+
 
 
 
@@ -27,7 +38,9 @@ db = SQLAlchemy(app)
 cors = CORS(app)
 bootstrap = Bootstrap(app)
 recaptcha = ReCaptcha(app)
+mail = Mail(app)
 migrate = Migrate(app, db)
+
 
 
 from . import views
